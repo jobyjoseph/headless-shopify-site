214eb229dc25a9eedef338574118d0465b45617f
Joby Joseph <reachjoby@gmail.com>
2024-08-28 05:32:18 -0700

    Layout in progress

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         {/* TODO: show favicon from settings <link rel="icon" type="image/png" href="{{ settings.favicon | image_url: width: 32, height: 32 }}"></link> */}
         {/* TODO: preconnect if settings.type_header_font.system? and settings.type_body_font.system? <link rel="preconnect" href="https://fonts.shopifycdn.com" crossorigin/> */}
         {/* TODO: Load social meta tags */}
+        <Script src="/scripts/constants.js" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
